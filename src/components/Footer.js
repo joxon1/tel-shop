1dd04349996e76052cd022e221bab39109681a85
Joxongir <joxon1@gmail.com>
2022-04-06 01:11:19 +0500

    header-footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="footer__btn">
+        <button type="button" class="btn btn-dark">
+          HOME
+        </button>
+        <button type="button" class="btn btn-dark">
+          LIKE
+        </button>
+        <button type="button" class="btn btn-dark">
+          +
+        </button>
+        <button type="button" class="btn btn-dark">
+          LOGIN
+        </button>
+        <button type="button" class="btn btn-dark">
+          SETTINGS
+        </button>
+      </div>
+    </nav>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+export default function Header() {
+  return (
+    <div className="header__shop">
+      <div className="collapse" id="navbarToggleExternalContent">
+        <div className="bg-dark p-4">
+          <h5 className="text-white h4">Collapsed content</h5>
+          <span className="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+      </div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <h4 className="header__logo">-TelShop-</h4>
+        </div>
+      </nav>
+    </div>
+  );
+}
